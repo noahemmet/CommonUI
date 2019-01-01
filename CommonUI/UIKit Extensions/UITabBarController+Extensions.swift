01f6a3066e35380aad5147503c67a7209ad14667
Noah Emmet <emmet@mini-moose.local>
2018-12-31 17:07:35 -0800

    UITabBarController extensions

---
+++ CommonUI/UIKit Extensions/UITabBarController+Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  UITabBarController+Extensions.swift
+//  CommonUI
+//
+//  Created by Noah Emmet on 12/31/18.
+//  Copyright © 2018 Noah Emmet. All rights reserved.
+//
+
+import Foundation
+import Common
+
+public extension UITabBarController {
+	var orderedTabBarItemViews: [UIView] {
+		let interactionViews = tabBar.subviews.filter({$0.isUserInteractionEnabled})
+		return interactionViews.sorted(by: {$0.frame.minX < $1.frame.minX})
+	}
+}
