a35a3a52c8b55cb12a4cf2f42237b25745b5e225
Noah Emmet <emmet@mini-moose.local>
2019-01-08 13:20:44 -0800

    cell wrapping size

--- CommonUI/ViewWrapping/UICollectionView+ViewWrapping.swift
+++ CommonUI/ViewWrapping/UICollectionView+ViewWrapping.swift
@@ -45,6 +45,16 @@ open class WrapperCollectionCell<View: UIView>: UICollectionViewCell, ViewWrappi
         
         wrappedView.widthAnchor.constraint(equalToConstant: contentView.frame.width).isActive = true
     }
+	
+	override open func preferredLayoutAttributesFitting(_ layoutAttributes: UICollectionViewLayoutAttributes) -> UICollectionViewLayoutAttributes {
+		setNeedsLayout()
+		layoutIfNeeded()
+		let size = contentView.systemLayoutSizeFitting(layoutAttributes.size)
+		var frame = layoutAttributes.frame
+		frame.size.height = ceil(size.height)
+		layoutAttributes.frame = frame
+		return layoutAttributes
+	}
     
     public var isSelectable: Bool { 
         get { return viewWrapper.isSelectable }
