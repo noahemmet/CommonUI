e004a9935bd167dc150d271969dd118c67f5b824
Noah Emmet <emmet@mini-moose.local>
2019-02-02 16:03:31 -0800

    Keyboard.swift

---
+++ CommonUI/Keyboard.swift
@@ -0,0 +1,15 @@
+//
+//  Keyboard.swift
+//  CommonUI
+//
+//  Created by Noah Emmet on 2/1/19.
+//  Copyright © 2019 Noah Emmet. All rights reserved.
+//
+
+import Foundation
+
+public enum Keyboard {
+	public static let defaultExpectedHeight: CGFloat = 253
+	public static let minimumExpectedHeight: CGFloat = 130
+	public static var lastKnownHeight: CGFloat = defaultExpectedHeight
+}
