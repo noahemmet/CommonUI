75819ec771bd09cf1df58c1cbec9250f12a6540f
Noah Emmet <noah@noahemmet.com>
2019-04-24 23:43:15 -0700

    cleanup

--- CommonUI/UIKit Replacements/TextView.swift
+++ CommonUI/UIKit Replacements/TextView.swift
@@ -115,7 +115,7 @@ open class TextView: UITextView, ViewModelConfigurable {
     }
     
     private func updateConstraintsForPlaceholderLabel() {
-		placeholderConstraints.top.constant = textContainerInset.top //- 1
+		placeholderConstraints.top.constant = textContainerInset.top
 		placeholderConstraints.bottom.constant = textContainerInset.bottom
 		placeholderConstraints.leading.constant = textContainerInset.left
 		placeholderConstraints.trailing.constant = textContainerInset.right
