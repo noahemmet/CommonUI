7d83bc10ea406e6aaa0c3a386deef44d2253d28e
Noah Emmet <noah@noahemmet.com>
2019-04-30 20:43:28 -0700

    env

---
+++ CommonUI/Env.swift
@@ -0,0 +1,15 @@
+//
+//  Env.swift
+//  CommonUI
+//
+//  Created by Noah Emmet on 4/30/19.
+//  Copyright © 2019 Noah Emmet. All rights reserved.
+//
+
+import Foundation
+
+public enum Env {
+	private static let flip: Bool = false
+	public static let v1: Bool = !flip
+	public static let v2: Bool = flip
+}
