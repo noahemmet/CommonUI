94220bafedc55fd6a1dc9bbddba27515ae513df7
Noah Emmet <noah@noahemmet.com>
2019-07-05 23:13:43 -0700

    5.0

--- Package.swift
+++ Package.swift
@@ -1,14 +1,10 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "CommonUI",
-	platforms: [
-		.iOS(.v13),
-		.macOS(.v10_14)
-	],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
