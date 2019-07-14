1b225221e0dcfe7f2aa82444ce390b1e8cf7e632
Noah Emmet <noah@noahemmet.com>
2019-07-13 19:16:44 -0700

    uicolor inverse

--- Sources/CommonUI/UIKit Extensions/UIColor+Extensions.swift
+++ Sources/CommonUI/UIKit Extensions/UIColor+Extensions.swift
@@ -32,6 +32,28 @@ public extension UIColor {
 			return nil
 		}
 	}
+	
+	var inverse: UIColor {
+		var alpha: CGFloat = 1.0
+		
+		var white: CGFloat = 0.0
+		if self.getWhite(&white, alpha: &alpha) {
+			return UIColor(white: 1.0 - white, alpha: alpha)
+		}
+		
+		var hue: CGFloat = 0.0, saturation: CGFloat = 0.0, brightness: CGFloat = 0.0
+		if self.getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: &alpha) {
+			return UIColor(hue: 1.0 - hue, saturation: 1.0 - saturation, brightness: 1.0 - brightness, alpha: alpha)
+		}
+		
+		var red: CGFloat = 0.0, green: CGFloat = 0.0, blue: CGFloat = 0.0
+		if self.getRed(&red, green: &green, blue: &blue, alpha: &alpha) {
+			return UIColor(red: 1.0 - red, green: 1.0 - green, blue: 1.0 - blue, alpha: alpha)
+		}
+		
+		return self
+	}
+
     
     func shuffled(within maxPercentage: CGFloat) -> UIColor! {
         
