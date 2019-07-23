09d077e93ef974ce458b0f951b9710299d3e97b4
Noah Emmet <noah@noahemmet.com>
2019-07-23 00:21:21 -0700

    UIUserInterfaceStyle toggle

---
+++ Sources/CommonUI/UIKit Extensions/UITraitCollection+Extensions.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+public extension UIUserInterfaceStyle {
+	mutating func toggle()  {
+		switch self {
+		case .light, .unspecified:
+			self = .dark
+		case .dark:
+			fallthrough
+		@unknown default:
+			self = .light
+		}
+	}
+}
