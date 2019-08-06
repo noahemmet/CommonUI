4adb484c106a6ce2caf48f8ab16a4248defcc548
Noah Emmet <noah@noahemmet.com>
2019-08-06 15:37:42 -0700

    go(after delay

--- Sources/CommonUI/DebugNav.swift
+++ Sources/CommonUI/DebugNav.swift
@@ -10,10 +10,10 @@ import Foundation
 import UIKit
 
 public enum DebugNav {
-	public static func go(_ handler: @escaping () -> Void) {
+	public static func go(after delay: TimeInterval = 0, _ handler: @escaping () -> Void) {
 		#if DEBUG
 		if Platform.isSimulator {
-			DispatchQueue.main.async {
+			DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
 				handler()
 			}
 		}
