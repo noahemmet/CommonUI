dfda8955630dbd59be39f4b72b04254631e85183
Noah Emmet <noah@noahemmet.com>
2019-10-23 23:39:11 -0700

    pushing

--- Sources/CommonUI/UIKit Extensions/UINavigationController+Extensions.swift
+++ Sources/CommonUI/UIKit Extensions/UINavigationController+Extensions.swift
@@ -1,26 +1,18 @@
-//
-//  UINavigationController+Extensions.swift
-//  CommonUI
-//
-//  Created by Noah Emmet on 11/16/18.
-//  Copyright © 2018 Sticks. All rights reserved.
-//
-
 import Foundation
 import UIKit
 
 public extension UINavigationController {
-    func pushNewStack(startingWith viewController: UIViewController, animated: Bool) {
-        let rootViewController = viewControllers[0]
-        setViewControllers([rootViewController, viewController], animated: animated)
-    }
+  func pushNewStack(startingWith viewController: UIViewController, animated: Bool) {
+    let rootViewController = viewControllers[0]
+    setViewControllers([rootViewController, viewController], animated: animated)
+  }
 }
 
 public extension UIViewController {
-	func setPopSlideEnabled(_ enabled: Bool) {
-		guard let navController = navigationController as? NavigationController else {
-			fatalError("navigationController superclass must be of type NavigationController")
-		}
-		navController.setPopSlide(to: enabled, for: self)
-	}
+  func setPopSlideEnabled(_ enabled: Bool) {
+    guard let navController = navigationController as? NavigationController else {
+      fatalError("navigationController superclass must be of type NavigationController")
+    }
+    navController.setPopSlide(to: enabled, for: self)
+  }
 }
